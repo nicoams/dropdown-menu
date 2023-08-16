092e0abfa101ac808ee353e46cfd85f951dd6181
nicoams <117552342+nicoams@users.noreply.github.com>
2023-08-16 17:15:02 -0300

    updates App

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <picture>
             <source
               media="(min-width:1000px)"
-              srcSet="/assets/images/image-hero-desktop.png"
+              srcSet={process.env.PUBLIC_URL + "/assets/images/image-hero-desktop.png"}
             ></source>
             <img src="/assets/images/image-hero-mobile.png" alt="" />
           </picture>
